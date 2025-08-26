2830ac2df9801a5cf2942fb98ca01b7969479b41
Cauazinkj <cauadepaulaneves@outlook.com>
2025-08-26 18:55:47 -0300

    feat: adicionei algumas verificações caso o código contenha algum erro e verificação de sucesso

--- src/api/Api/Principal/PrincipalComBusca.java
+++ src/api/Api/Principal/PrincipalComBusca.java
@@ -5,7 +5,6 @@ import api.Api.Modelos.TituloOmdb;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
